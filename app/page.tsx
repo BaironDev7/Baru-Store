20a48e5e41bfd73beddbe57a377b8eed9c1ad4d3
BaironDev <54222125+BaironDev7@users.noreply.github.com>
2024-10-15 14:49:36 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6 text-blue-700">Welcome to My Store</h1>
+      <h1 className="text-3xl font-bold mb-6 text-blue-700">Welcome to Baru Store</h1>
       <Link href="/products" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
         Go to Products
       </Link>
